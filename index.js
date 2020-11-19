ab305a95c27da001998fb2186f595afcda737689
sarabastian <seb118@georgetown.edu>
2020-11-19 09:54:32 -0500

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 function fetchBooks() {
+  return fetch('https://anapioficeandfire.com/api/books').then(function(response) {
+  return response.json();
+}).then(function(json) {
+  
+  renderBooks(json)
+});
 
 }
 
